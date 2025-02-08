900dc8c4f6ee567b5cb6159531c732f913dcf5ce
Gvaldz <233415@ids.upchiapas.edu.mx>
2025-02-07 23:14:02 -0600

    add: Se agrega cors

--- src/server/server.go
+++ src/server/server.go
@@ -1,9 +1,11 @@
 package server
 
 import (
-	"github.com/gin-gonic/gin"
 	doctorRouters "clinica/src/internal/doctors/infraestructure/routers"
 	patientRouters "clinica/src/internal/patients/infraestructure/routers"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func Run(
@@ -11,6 +13,14 @@ func Run(
 	patientRoutes *patientRouters.PatientRoutes,
 ) {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, 
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	
+	}))
 
 	doctorRoutes.AttachRoutes(r)
 	patientRoutes.AttachRoutes(r)
